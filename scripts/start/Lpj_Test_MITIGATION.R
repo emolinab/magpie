79fb045baa5c1c8a7f38227be028cfd678324b90
emolinab <mbacca@pik-potsdam.de>
2021-03-18 22:13:58 +0100

    setting calibration files

--- scripts/start/Lpj_Test_MITIGATION.R
+++ scripts/start/Lpj_Test_MITIGATION.R
@@ -15,25 +15,29 @@ realization<-c("sticky_feb18")
 #realization<-c("mixed_feb17")
 climate<-c("cc","nocc")
 sticky_modes<-c("regional","dynamic","free")
+inputs<-c("rev4.58_h12_validation.tgz",
+         "additional_data_rev3.98.tgz",
+         "rev4.59+mrmagpie_LPJmL_new2_h12_5e4fb8e4d1e7450f19bf2d682b4a8338_cellularmagpie_debug.tgz",
+         "rev4.59+mrmagpie_LPJmL_new2_h12_magpie_debug.tgz",
+         "additional_sticky.tgz")
+calib<-c("calibration_H12_sticky_regional_18Mar21.tgz",
+        "calibration_H12_sticky_dynamic_18Mar21.tgz",
+        "calibration_H12_sticky_free_18Mar21.tgz")
 
 
 
     for(r in realization){
       for(c in climate){
-            for (so in sticky_modes){
+            for (so in 1:length(sticky_modes)){
 
         #Title
-        cfg$title<-paste0("Sticky_LPJmL5_",so,"_",c,"_")
+        cfg$title<-paste0("Sticky_LPJmL5_",sticky_modes[so],"_",c,"_")
 
         #configuration of scenarios
         cfg <- setScenario(cfg,c)
 
         #this could be extended for different gcms and rcps
-          cfg$input <- c("rev4.58_h12_validation.tgz",
-                   "additional_data_rev3.98.tgz",
-                   "rev4.59+mrmagpie_LPJmL_new2_h12_5e4fb8e4d1e7450f19bf2d682b4a8338_cellularmagpie_debug.tgz",
-                   "rev4.59+mrmagpie_LPJmL_new2_h12_magpie_debug.tgz",
-                   "additional_sticky.tgz")
+          cfg$input <- c(inputs,calib[so])
 
 
         #Force download? recalibrate?
@@ -44,7 +48,7 @@ sticky_modes<-c("regional","dynamic","free")
         cfg$gms$factor_costs <- r
 
         #Way to operate sticky
-        cfg$gms$c38_sticky_mode  <- so
+        cfg$gms$c38_sticky_mode  <- sticky_modes[so]
 
         #priority
         #cfg$qos <- "priority"
