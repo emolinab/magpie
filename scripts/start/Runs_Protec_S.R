82dcf55fcdc21ab540c73a1fa1796ffea259cfed
emolinab <mbacca@pik-potsdam.de>
2021-04-07 16:47:25 +0200

    no cc for normal and depreciaiton runs

--- scripts/start/Runs_Protec_S.R
+++ scripts/start/Runs_Protec_S.R
@@ -19,7 +19,7 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 realization<-c("sticky_feb18")
-sticky_modes<-c("free")
+sticky_modes<-c("free","dynamic")
 inputs <- c("rev4.59_8f7b9423_validation_debug.tgz",
          "additional_data_rev3.99.tgz",
          "rev4.59_8f7b9423_024608f1_cellularmagpie_debug.tgz",
@@ -28,50 +28,50 @@ inputs <- c("rev4.59_8f7b9423_validation_debug.tgz",
          "ZabelPatchH13.tgz"
          )
 
-climate<-c("cc")
+climate<-c("nocc")
 
-he_calib<-c("calibration_H12_HE_sticky_feb18_06Apr21.tgz",
-            "calibration_H12_HE_mixed_feb17_06Apr21.tgz")
+#he_calib<-c("calibration_H12_HE_sticky_feb18_06Apr21.tgz",
+#            "calibration_H12_HE_mixed_feb17_06Apr21.tgz")
 # Half Earth
-aux<-1
-for (i in realization){
+#aux<-1
+#for (i in realization){
 #  if(i != "sticky_feb18"){
-  for (so in sticky_modes){
+#  for (so in sticky_modes){
 
-cfg$title <- paste0("LPj5_",i,"_HalfEarth_free_",climate,"_")
+#cfg$title <- paste0("LPj5_",i,"_HalfEarth_free_",climate,"_")
 
 #configuration of scenarios
-cfg <- setScenario(cfg,climate)
+#cfg <- setScenario(cfg,climate)
 
 #inputs
-cfg$input <- c(inputs,he_calib[aux])
-cfg$force_download <- TRUE
-cfg$recalibrate <- FALSE
+#cfg$input <- c(inputs,he_calib[aux])
+#cfg$force_download <- TRUE
+#cfg$recalibrate <- FALSE
 
 #Selects factor costs realization
-cfg$gms$factor_costs <- i
-cfg$gms$c38_sticky_mode  <- so
+#cfg$gms$factor_costs <- i
+#cfg$gms$c38_sticky_mode  <- so
 
 # Half earth scenario
-cfg$gms$c35_protect_scenario <- "HalfEarth"
+#cfg$gms$c35_protect_scenario <- "HalfEarth"
 
-cfg$output <- c("rds_report")
+#cfg$output <- c("rds_report")
 
-start_run(cfg=cfg)
+#start_run(cfg=cfg)
 
-aux<-aux+1
-}}
+#aux<-aux+1
+#}}
 #}
 #### Normal runs with changes substitution and patrick's suitable area
-normal_calib<-c("calibration_H12_calibLPJ5_sticky_feb18_06Apr21.tgz",
-            "calibration_H12_calibLPJ5_mixed_feb17_07Apr21.tgz")
+#normal_calib<-c("calibration_H12_calibLPJ5_sticky_feb18_06Apr21.tgz",
+#            "calibration_H12_calibLPJ5_mixed_feb17_07Apr21.tgz")
 # Half Earth
 aux<-1
 
 for (i in realization){
   for (so in sticky_modes){
 
-    cfg$title <- paste0("LPJ5_",i,"_free_Normal_",so,"_",climate,"_")
+    cfg$title <- paste0("LPJ5_",i,"_Normal_",so,"_",climate,"_")
 
     #configuration of scenarios
     cfg <- setScenario(cfg,climate)
@@ -96,50 +96,21 @@ for (i in realization){
 ######
 #depreciation
 
-#dep_calib<-c("calibration_H12_dep_0_07Apr21.tgz",
-#            "calibration_H12_dep_0.01_07Apr21.tgz",
-#            "calibration_H12_dep_0.1_07Apr21.tgz",
-#            "calibration_H12_dep_1_07Apr21.tgz")
+dep_calib<-c("calibration_H12_dep_0_07Apr21.tgz",
+            "calibration_H12_dep_0.01_07Apr21.tgz",
+            "calibration_H12_dep_0.1_07Apr21.tgz",
+            "calibration_H12_dep_1_07Apr21.tgz")
 # Half Earth
-#aux<-1
-#depreciation<-c(0,0.01,0.1,1)
+aux<-1
+depreciation<-c(0,0.01,0.1,1)
 
-#for (d in depreciation){
-
-
-#cfg$title <- paste0("LPJ5_StickyDy_Dep_",d,"_",climate,"_")
-#cfg$input <- c(inputs,dep_calib[aux])
-
-#configuration of scenarios
-#cfg <- setScenario(cfg,climate)
-#
-#cfg$force_download <- TRUE
-#cfg$recalibrate <- FALSE
-
-#Selects factor costs realization
-#cfg$gms$factor_costs <- "sticky_feb18"
-#cfg$gms$c38_sticky_mode  <- "dynamic"
-#cfg$gms$s38_depreciation_rate <- d
+for (d in depreciation){
 
 
-#cfg$output <- c("rds_report")
+cfg$title <- paste0("LPJ5_StickyDy_Dep_",d,"_",climate,"_")
+cfg$input <- c(inputs,dep_calib[aux])
 
-#start_run(cfg=cfg)
-
-#aux<-aux+1
-#}
-
-###### Protect existing cropland area
-calib<-"calibration_H12_calibLPJ5_sticky_feb18_06Apr21.tgz"
-percent<-c(0.1,0.3,0.5,0.7,0.9)
-
-for (p in percent){
-
-
-cfg$title <- paste0("LPJ5_StickyDy_ProtectLand_",p,"_",climate,"_")
-cfg$input <- c(inputs,calib)
-
-#configuration of scenarios
+configuration of scenarios
 cfg <- setScenario(cfg,climate)
 
 cfg$force_download <- TRUE
@@ -147,14 +118,43 @@ cfg$recalibrate <- FALSE
 
 #Selects factor costs realization
 cfg$gms$factor_costs <- "sticky_feb18"
-cfg$gms$c38_sticky_mode  <- "free"
-cfg$gms$s38_depreciation_rate <- 0.05
+cfg$gms$c38_sticky_mode  <- "dynamic"
+cfg$gms$s38_depreciation_rate <- d
 
-cfg$gms$c30_protect_crop  <- TRUE
-cfg$gms$s30_perc_protected  <- p
 
 cfg$output <- c("rds_report")
 
 start_run(cfg=cfg)
 
+aux<-aux+1
 }
+
+###### Protect existing cropland area
+#calib<-"calibration_H12_calibLPJ5_sticky_feb18_06Apr21.tgz"
+#percent<-c(0.1,0.3,0.5,0.7,0.9)
+
+#for (p in percent){
+
+
+#cfg$title <- paste0("LPJ5_StickyDy_ProtectLand_",p,"_",climate,"_")
+#cfg$input <- c(inputs,calib)
+
+#configuration of scenarios
+#cfg <- setScenario(cfg,climate)
+
+#cfg$force_download <- TRUE
+#cfg$recalibrate <- FALSE
+
+#Selects factor costs realization
+#cfg$gms$factor_costs <- "sticky_feb18"
+#cfg$gms$c38_sticky_mode  <- "free"
+#cfg$gms$s38_depreciation_rate <- 0.05
+
+#cfg$gms$c30_protect_crop  <- TRUE
+#cfg$gms$s30_perc_protected  <- p
+
+#cfg$output <- c("rds_report")
+
+#start_run(cfg=cfg)
+
+#}
