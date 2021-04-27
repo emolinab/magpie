5855a934170cff7f9896e3aac282b21ed0668de2
emolinab <mbacca@pik-potsdam.de>
2021-04-27 15:48:42 +0200

    typo calibration

--- scripts/start/LPJ_SP_tests.R
+++ scripts/start/LPJ_SP_tests.R
@@ -34,7 +34,7 @@ input1[["SP_new"]] <- c("rev4.59SmashingPumpkins_h12_validation_debug.tgz",
 calibration_mixed[["mixed_feb17"]][["SP_new"]]<-"calibration_H12_irrigSP_mixed_feb17__23Apr21.tgz"
 
 #calibration_sticky[["SP_new"]][["free"]]<-"calibration_SP_new_sticky_feb18_fr__20Apr21.tgz"
-calibration_sticky[["SP_new"]][["dynamic"]]<-"calibration_H12_irrigSP_sticky_feb18_free__27Apr21.tgz"
+calibration_sticky[["SP_new"]][["free"]]<-"calibration_H12_irrigSP_sticky_feb18_free__27Apr21.tgz"
 
   for(sp in SP1){
     for(r in realization){
