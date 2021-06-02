99becfc80c5c2a7f8781fb1dcce7b5d6931083b8
emolinab <mbacca@pik-potsdam.de>
2021-06-02 13:33:19 +0200

    pDSSAT runs calibration

--- scripts/start/extra/recalibrate_inputs.R
+++ scripts/start/extra/recalibrate_inputs.R
@@ -24,18 +24,18 @@ sticky_modes<-c("dynamic","free")
 #sticky_modes<-c("")
 
 combo<-c(#"7p0_CYGMA_GFDL",
-        "8p5_CYGMA_UKESM")
-        #"8p5_pDSSAT_UKESM",
+        #"8p5_CYGMA_UKESM")
+        "8p5_pDSSAT_UKESM")
         #"8p5_EPIC_UKESM")
         #"7p0_EPIC_GFDL")
 
 hashes_combos<-as.character(c(#"c6f10324",
-                 "e61ed473"))
-                 #"256c3ab7",
+                 #"e61ed473"
+                 "256c3ab7"))
                  #"c0547439"))
                  #"669b91c3")
 
-names_sce<-c("Variable")
+names_sce<-c("Capital+Variable","Inputs_Variable")
 
 input<-c("additional_data_rev4.04.tgz",
                "rev4.59_h12_magpie.tgz",
