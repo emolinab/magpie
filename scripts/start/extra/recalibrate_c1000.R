d7924904682d4ae64424c84335674e00ec06411c
emolinab <mbacca@pik-potsdam.de>
2022-02-27 10:00:52 +0100

    cleanup c1000

--- scripts/start/extra/recalibrate_c1000.R
+++ scripts/start/extra/recalibrate_c1000.R
@@ -20,7 +20,7 @@ source("config/default.cfg")
 
 cfg$input <- c(cellular    = "rev4.65+ISIMIP_140122_8f7b9423_f0244510_cellularmagpie_c1000_IPSL-CM6A-LR-ssp126_lpjml-c5bacf3f.tgz",
                regional    = "rev4.65+ISIMIP_140122_8f7b9423_magpie.tgz",
-               validation  = "rev4.65+ISIMIP_140122_8f7b9423_validation",
+               validation  = "rev4.65+ISIMIP_140122_8f7b9423_validation.tgz",
                additional  = "additional_data_rev4.07.tgz",
                calibration = "calibration_H13_ISIMIP_150122_15Jan22.tgz"
                )
@@ -31,22 +31,14 @@ cfg$gms$factor_costs<- "sticky_feb18"
 cfg$gms$c38_sticky_mode <- "dynamic"
 cfg$force_download <- TRUE
 
-#get trade pattern from low resolution run with c200
-gdx<-"/p/projects/magpie/data/ISIMIP/ISIMIP_15022022/magpie/output/c200_260122/ISIMIP_260122_ssp126_IPSL-CM6A-LR_cc/fulldata.gdx"
-ov_prod_reg <- readGDX(gdx,"ov_prod_reg",select=list(type="level"))
-ov_supply <- readGDX(gdx,"ov_supply",select=list(type="level"))
-f21_trade_balance <- ov_prod_reg - ov_supply
-write.magpie(round(f21_trade_balance,6),paste0("modules/21_trade/input/f21_trade_balance.cs3"))
-
-
 cfg$results_folder <- "output/:title:"
 cfg$recalibrate <- FALSE
 cfg$recalibrate_landconversion_cost <- TRUE
-cfg$title <- "calib_c1000_H13_250222"
+cfg$title <- "calib_c1000_H13_270222"
 cfg$output <- c("rds_report")
 
 #parallel
 cfg$qos <- "medium"
 
 start_run(cfg,codeCheck=FALSE)
-magpie4::submitCalibration("H13_c1000_250222")
+magpie4::submitCalibration("H13_c1000_270222")
