ee5dddeba033b5084a1096ca43f29568893b703d
emolinab <mbacca@pik-potsdam.de>
2022-02-28 15:04:11 +0100

    submit medium

--- scripts/run_submit/submit_medium.sh
+++ scripts/run_submit/submit_medium.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=mag-run
 #SBATCH --output=slurm.log
 #SBATCH --mail-type=END
-#SBATCH --cpus-per-task=256
+#SBATCH --cpus-per-task=3
 #SBATCH --partition=standard
-
+#SBATCH --mem=60000
 
 Rscript submit.R
