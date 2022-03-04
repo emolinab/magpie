e88464099f9fe08c87109c33f2a4c441813642b5
emolinab <mbacca@pik-potsdam.de>
2022-03-04 16:35:08 +0100

    calibration with forestry

--- scripts/start/extra/recalibrate_c1000.R
+++ scripts/start/extra/recalibrate_c1000.R
@@ -25,7 +25,7 @@ cfg$input <- c(cellular    = "rev4.65+ISIMIP_140122_8f7b9423_f0244510_cellularma
                calibration = "calibration_H13_ISIMIP_150122_15Jan22.tgz"
                )
 
-
+  cfg <- gms::setScenario(cfg,c("ForestryEndo"))
 cfg$gms$s13_ignore_tau_historical <- 1 #ignoring historical tau == 1
 cfg$gms$factor_costs<- "sticky_feb18"
 cfg$gms$c38_sticky_mode <- "dynamic"
@@ -34,7 +34,7 @@ cfg$force_download <- TRUE
 cfg$results_folder <- "output/:title:"
 cfg$recalibrate <- FALSE
 cfg$recalibrate_landconversion_cost <- TRUE
-cfg$title <- "calib_c1000_H13_Par"
+cfg$title <- "calib_c1000_H13_ParFor"
 cfg$output <- c("rds_report")
 
 #parallel
@@ -53,4 +53,4 @@ cfg$qos <- "medium"
 
 
 start_run(cfg,codeCheck=FALSE)
-magpie4::submitCalibration("H13_c1000_Par")
+magpie4::submitCalibration("H13_c1000_ParFor")
