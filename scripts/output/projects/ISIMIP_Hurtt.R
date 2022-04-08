bd87ec22773ec47ab0d4ff69a658e6c834bc4bc3
emolinab <mbacca@pik-potsdam.de>
2022-04-08 13:54:18 +0200

    ssp585nocc scenario

--- scripts/output/projects/ISIMIP_Hurtt.R
+++ scripts/output/projects/ISIMIP_Hurtt.R
@@ -40,7 +40,7 @@ dir<-outputdir
 save_path<-"/p/projects/magpie/transfers/HurttMaps-010422"
     if(!dir.exists(save_path)) dir.create(save_path)
 
-scenarios<-c("ssp126", "ssp370", "ssp585")
+scenarios<-c("ssp126", "ssp370", "ssp585noco2") #"ssp585",
 
 gcms<-c("GFDL-ESM4","MRI-ESM2-0","UKESM1-0-LL","MPI-ESM1-2-HR","IPSL-CM6A-LR","2015soc")
 
