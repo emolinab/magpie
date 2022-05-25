801cd39524d3e709b7afdc44193ac21e3773c7c7
emolinab <mbacca@pik-potsdam.de>
2022-05-25 07:27:41 +0200

    noco2 run

--- scripts/output/projects/ISIMIP_Hurtt.R
+++ scripts/output/projects/ISIMIP_Hurtt.R
@@ -40,7 +40,7 @@ dir<-outputdir
 save_path<-"/p/projects/magpie/transfers/ISIMIP3b_MAgPIE_LUH_220524/"
     if(!dir.exists(save_path)) dir.create(save_path)
 
-scenarios<-c("ssp126", "ssp370", "ssp585") #, "ssp585noco2"
+scenarios<-c("ssp585noco2") #,  "ssp126", "ssp370", "ssp585"
 
 gcms<-c("GFDL-ESM4","MRI-ESM2-0","UKESM1-0-LL","MPI-ESM1-2-HR","IPSL-CM6A-LR","2015soc")
 
