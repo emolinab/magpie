12af7daa7cc6ba604f187889f82eafaec930b7af
emolinab <mbacca@pik-potsdam.de>
2022-08-27 12:58:26 +0200

    recalibrate script new runs

--- scripts/start/Paper_2/ISIMIP_recalibrate.R
+++ scripts/start/Paper_2/ISIMIP_recalibrate.R
@@ -18,10 +18,10 @@ source("scripts/start_functions.R")
 #start MAgPIE run
 source("config/default.cfg")
 
-cfg$input <- c(cellular    = "rev4.64+ISIMIP_300921_noDeb_h12_884ea77c_cellularmagpie_c200_GFDL-ESM4-ssp126_lpjml-e8ab65dd.tgz",
-               regional    = "rev4.64_h12_magpie.tgz",
-               validation  = "rev4.64+ISIMIP_300921_noDeb_h12_validation.tgz",
-               additional  = "additional_data_rev4.04.tgz")
+cfg$input <- c(cellular    = "rev4.65+Paper_270822_pDSSAT_MPI-ESM1-2-HR_8f7b9423_6fc6400e_cellularmagpie_c200_MPI-ESM1-2-HR-ssp126_lpjml-8e6c5eb1_isimip-aa82a2e6.tgz",
+               regional    = "rev4.65+ISIMIP_140122_8f7b9423_magpie.tgz",
+               validation  = "rev4.65+ISIMIP_140122_8f7b9423_validation",
+               additional  = "additional_data_rev4.07.tgz",)
 
 cfg$gms$s13_ignore_tau_historical <- 1 #ignoring historical tau == 1
 cfg$gms$factor_costs<- "sticky_feb18"
@@ -30,9 +30,10 @@ cfg$force_download <- TRUE
 
 cfg$results_folder <- "output/calibration/:title:"
 cfg$recalibrate <- TRUE
-cfg$title <- "calib_run_FOCUS"
+cfg$recalibrate_landconversion_cost <- TRUE
+cfg$title <- "calib_Paper2"
 cfg$gms$c_timesteps <- 1
 cfg$output <- c("rds_report")
 cfg$sequential <- TRUE
 start_run(cfg,codeCheck=FALSE)
-magpie4::submitCalibration("H12_ISIMIP")
+magpie4::submitCalibration("H13_Paper")
