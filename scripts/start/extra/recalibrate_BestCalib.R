0c8bb143fc0093f55e797d2ecd8ada6da8137a5d
emolinab <mbacca@pik-potsdam.de>
2023-01-06 16:51:03 +0100

    calib sticky

--- scripts/start/extra/recalibrate_BestCalib.R
+++ scripts/start/extra/recalibrate_BestCalib.R
@@ -18,7 +18,7 @@ source("scripts/start_functions.R")
 # get default settings
 source("config/default.cfg")
 
-realizations <- c( "sticky_feb18") # "per_ton_fao_may22", "sticky_labor" is very similar to sticky_feb18. No extra calibration needed.
+realizations <- c("sticky_feb18") # "per_ton_fao_may22","sticky_labor" is very similar to sticky_feb18. No extra calibration needed.
 type <- NULL
 
 cfg$results_folder <- "output/:title:"
@@ -32,12 +32,12 @@ cfg$gms$c_timesteps <- "calib"
 
 for (r in realizations) {
   cfg$gms$factor_costs <- r
-best <- if (r != "per_ton_fao_may22") c(TRUE,FALSE) else FALSE
+best <- if (r != "per_ton_fao_may22") c(FALSE) else FALSE
 for (b in best){
       cfg$best_calib <- b
       cfg$title <- paste("calib_run", r, "bC",b, sep = "_")
     start_run(cfg)
-    magpie4::submitCalibration(paste("H12", r, fac_req, sep = "_"))
+  #  magpie4::submitCalibration(paste("H12", r, b, sep = "_"))
 
 }
 }
